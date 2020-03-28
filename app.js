e7146768ee29564b72819b1280114ba1f7fe9bba
Prakash <mahanthisatyaprakash@gmail.com>
2020-03-28 10:20:18 +0530

    Server on port 3000

---
+++ app.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res){
+  res.render("index");
+})
+
+app.listen(3000, function(){
+  console.log("Server is running on the port 3000");
+})
