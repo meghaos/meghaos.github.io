9047dc310f255b77bdc669eb554f70990b721119
Prakash <mahanthisatyaprakash@gmail.com>
2020-03-28 13:37:48 +0530

    Major changes

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
+
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
   res.render("index");
-})
+});
 
 app.listen(3000, function(){
   console.log("Server is running on the port 3000");
-})
+});
